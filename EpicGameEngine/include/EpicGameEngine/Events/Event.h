1037d315e925c6e50ad7dcc950d9f7e3292d8a3d
Rishan Thangaraj <rishanthangaraj@gmail.com>
2022-06-11 17:58:20 -0400

    Create Basic Event Header

---
+++ EpicGameEngine/include/EpicGameEngine/Events/Event.h
@@ -0,0 +1,55 @@
+#pragma once
+
+namespace EpicGameEngine
+{
+	// TODO: Create derived Event classes based on these EventTypes.
+
+	/*
+		Mouse events should store what mouse button was pressed
+		Keyboard events should store what key was pressed
+	*/
+	enum class EventType
+	{
+		None = 0,
+		MousePressed, MouseReleased, KeyPressed, KeyReleased,
+		WindowClose, WindowResize
+	}
+
+	class Event
+	{
+		Event();
+		virtual ~Event();
+
+		virtual EventType EventType() const = 0;
+		virtual void toString() const = 0;
+	};
+
+	// Use to "dispatch" events to their respective function handlers. 
+	class EventDispatcher
+	{
+		EventDispatcher(Event& event)
+			: event(event)
+		{
+		}
+
+		//	This is from TheCherno - I honestly don't know how it works 
+		template<typename T, typename F>
+		bool Dispatch(const F& func)
+		{
+			if (event.GetEventType() == T::GetStaticType())
+			{
+				event.Handled |= func(static_cast<T&>(event));
+				return true;
+			}
+			return false;
+		}
+	private:
+		Event& event;
+	};
+	
+	// TODO: Implement this after derived EventTypes are created.
+	Event& SDL_Event_to_Event(SDL_Event* event)
+	{
+
+	}
+}
