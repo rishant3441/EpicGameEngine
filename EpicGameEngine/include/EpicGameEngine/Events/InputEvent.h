c27ce18d02446c24efe0795c09c42bdd91d014d1
Rishan Thangaraj <rishanthangaraj@gmail.com>
2022-06-12 17:01:39 -0400

    Add some TODOs

--- EpicGameEngine/include/EpicGameEngine/Events/InputEvent.h
+++ EpicGameEngine/include/EpicGameEngine/Events/InputEvent.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <EpicGameEngine/Events/Event.h>
 
+// TODO: Add all of the derived input events defined in the EventTypes enum here.
 namespace EpicGameEngine
 {
 	
--- EpicGameEngine/include/EpicGameEngine/Events/WindowEvent.h
+++ EpicGameEngine/include/EpicGameEngine/Events/WindowEvent.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <EpicGameEngine/Events/Event.h>
 
+// TODO: Add all of the derived window events defined in the EventTypes enum here.
 namespace EpicGameEngine
 {
 
