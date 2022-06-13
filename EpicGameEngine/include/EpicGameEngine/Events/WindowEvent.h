685dddb5243d6cce7a3bdae3ea69ef1dfd53c702
Rishan Thangaraj <rishanthangaraj@gmail.com>
2022-06-12 22:09:14 -0400

    Create Basic WindowEvent Classes

--- EpicGameEngine/include/EpicGameEngine/Events/WindowEvent.h
+++ EpicGameEngine/include/EpicGameEngine/Events/WindowEvent.h
@@ -4,5 +4,29 @@
 // TODO: Add all of the derived window events defined in the EventTypes enum here.
 namespace EpicGameEngine
 {
+    class WindowCloseEvent : public Event
+    {
+    public:
+        WindowCloseEvent() {}
+        ~WindowCloseEvent() {}
 
+        virtual EventType GetEventType() { return EventType::WindowClose; }
+    };
+
+    class WindowResizeEvent : public Event
+    {
+    public:
+        WindowResizeEvent(unsigned int width, unsigned int height)
+        : width(width), height(height)
+        {
+
+        }
+
+        unsigned int GetNewWidth() { return width; }
+        unsigned int GetNewHeight() { return height; }
+
+    private:
+        unsigned int width;
+        unsigned int height; 
+    };
 }
