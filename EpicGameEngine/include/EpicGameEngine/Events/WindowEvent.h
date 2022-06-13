f97e86a29971fac446b3b278a6f20bb6d7f39d38
Rishan Thangaraj <rishanthangaraj@gmail.com>
2022-06-13 09:52:30 -0400

    Update TODO

--- EpicGameEngine/include/EpicGameEngine/Events/WindowEvent.h
+++ EpicGameEngine/include/EpicGameEngine/Events/WindowEvent.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <EpicGameEngine/Events/Event.h>
 
-// TODO: Add all of the derived window events defined in the EventTypes enum here.
+// TODO: Write ToString() function potentially. 
 namespace EpicGameEngine
 {
     class WindowCloseEvent : public Event
