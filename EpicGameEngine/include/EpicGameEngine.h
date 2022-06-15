c09a696d9546500422b2c54996b32638f31fbb36
Rishan Thangaraj <rishanthangaraj@gmail.com>
2022-06-15 15:55:37 -0400

    Update Main Header

--- EpicGameEngine/include/EpicGameEngine.h
+++ EpicGameEngine/include/EpicGameEngine.h
@@ -8,8 +8,20 @@
 // -------------ENTRY POINT------------------
 #include <EpicGameEngine/EntryPoint.h>
 // ------------------------------------------
+// WINDOW:
 #include <EpicGameEngine/Window/Window.h>
 #ifdef EGE_PLATFORM_WINDOWS
 	#include <EpicGameEngine/Window/WindowsWindow.h>
 	#include <EpicGameEngine/Input/Input.h>
 #endif
+
+// LOGGING:
+#include <spdlog/spdlog.h>
+
+// EVENTS:
+#include <EpicGameEngine/Events/Event.h>
+
+// INPUT:
+#include <EpicGameEngine/Input/Input.h>
+#include <EpicGameEngine/Input/KeyCodes.h>
+#include <EpicGameEngine/Input/MouseCodes.h>
