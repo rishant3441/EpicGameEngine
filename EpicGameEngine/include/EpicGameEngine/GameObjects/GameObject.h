1e6cc172cae6d29055da6762528cf12da78e74f1
Rishan Thangaraj <rishanthangaraj@gmail.com>
2022-07-08 10:28:02 -0400

    Update GameObject with component support

--- EpicGameEngine/include/EpicGameEngine/GameObjects/GameObject.h
+++ EpicGameEngine/include/EpicGameEngine/GameObjects/GameObject.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <EpicGameEngine/ege_pch.h>
+#include <entt.hpp>
+#include "Scene.h"
 
 namespace EpicGameEngine
 {
@@ -7,16 +9,48 @@ namespace EpicGameEngine
     {
     public:
         GameObject() = default;
-        virtual ~GameObject() = default;
+        GameObject(entt::entity handle, Scene* scene);
+        GameObject(const GameObject&) = default;
 
-        virtual void Render() = 0;
-        virtual void Shutdown() = 0;
+        virtual void Shutdown() {}
         [[nodiscard]] auto GetName() const -> std::string { return name; }
          
 
         // doesn't actually delete class
         static void Destroy(GameObject& gameObject) { gameObject.Shutdown(); }
+
+        template<typename T, typename... Args>
+        T& AddComponent(Args&&... args)
+        {
+            assert(!HasComponent<T>() && "Entity already has this component!");
+            return scene->registry.emplace<T>(entityHandle, std::forward<Args>(args)...);
+        }
+
+        template<typename T>
+        T& GetComponent()
+        {
+            assert(HasComponent<T>() && "Entity does not have this component!");
+            return scene->registry.get<T>(entityHandle);
+        }
+
+        template<typename T>
+        bool HasComponent()
+        {
+            return scene->registry.all_of<T>(entityHandle);
+        }
+
+        template<typename T>
+        void RemoveComponent()
+        {
+            assert(HasComponent<T>() && "Entity does not have this component!");
+            scene->registry.remove<T>(entityHandle);
+        }
+
+        operator bool() const { return entityHandle != entt::null; }
+
     private:
-        std::string name; 
+        std::string name;
+        entt::entity entityHandle{ entt::null };
+        Scene* scene = nullptr;
     };
 }
