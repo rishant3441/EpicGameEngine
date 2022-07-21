e605a580a9ff0fd8fde5cef33f997cd7ed8068ff
Rishan Thangaraj <rishanthangaraj@gmail.com>
2022-07-21 12:35:29 -0400

    Add Refs

---
+++ EpicGameEditor/src/GameObjectsPanel.cpp
@@ -0,0 +1,8 @@
+//
+// Created by Rishan Thangaraj on 7/21/2022.
+//
+// Copyright (c) 2022, Rishan Thangaraj <rishanthangaraj@gmail.com> All rights reserved.
+// SPDX-License-Identifier: MIT
+//
+
+#include "GameObjectsPanel.h"
---
+++ EpicGameEditor/src/GameObjectsPanel.h
@@ -0,0 +1,15 @@
+//
+// Created by Rishan Thangaraj on 7/21/2022.
+//
+// Copyright (c) 2022, Rishan Thangaraj <rishanthangaraj@gmail.com> All rights reserved.
+// SPDX-License-Identifier: MIT
+//
+
+#pragma once
+
+
+class GameObjectsPanel
+{
+
+};
+
