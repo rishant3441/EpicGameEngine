be51491bbc9dfde19f2cc202a61707ca6bd111cd
Rishan Thangaraj <rishanthangaraj@gmail.com>
2022-07-21 09:23:31 -0400

    Add imgui.ini

---
+++ EpicGameEngine/src/EpicGameEngine/Renderer/Camera/Camera.cpp
@@ -0,0 +1,8 @@
+//
+// Created by Rishan Thangaraj on 7/19/2022.
+//
+// Copyright (c) 2022, Rishan Thangaraj <rishanthangaraj@gmail.com> All rights reserved.
+// SPDX-License-Identifier: MIT
+//
+
+#include "Camera.h"
